9786338946468f102fdda3e45fda147781d1122c
content Tingching0602 <contentanita2131love@gmail.com>
2022-02-17 22:25:33 +0800

    v1

---
+++ src/components/UserProfileCard.vue
@@ -0,0 +1,87 @@
+<template>
+  <div class="card mb-3">
+    <div class="row no-gutters">
+      <div class="col-md-4">
+        <img :src="user.image" height="300px" />
+      </div>
+      <div class="col-md-8">
+        <div class="card-body">
+          <h5 class="card-title">{{ user.name }}</h5>
+          <p class="card-text">
+            {{ user.email }}
+          </p>
+          <ul class="list-unstyled list-inline">
+            <li>
+              <strong>{{ user.Comments.length }}</strong> 已評論餐廳
+            </li>
+            <li>
+              <strong>{{ user.FavoritedRestaurants.length }}</strong>
+              收藏的餐廳
+            </li>
+            <li>
+              <strong>{{ user.Followings.length }}</strong> followings (追蹤者)
+            </li>
+            <li>
+              <strong>{{ user.Followers.length }}</strong> followers (追隨者)
+            </li>
+          </ul>
+          <template v-if="user.isCurrentUser">
+            <router-link
+              :to="{ name: 'user-edit', params: { id: user.id } }"
+              class="btn btn-primary"
+            >
+              Edit
+            </router-link>
+          </template>
+          <template v-else>
+            <button
+              v-if="isFollowed"
+              @click.stop.prevent="unFollow"
+              type="button"
+              class="btn btn-danger"
+            >
+              取消追蹤
+            </button>
+            <button
+              v-else
+              @click.stop.prevent="follow"
+              type="submit"
+              class="btn btn-primary"
+            >
+              追蹤
+            </button>
+          </template>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  props: {
+    initialUser: {
+      type: Object,
+      required: true,
+    },
+    initialIsFollowed: {
+      type: Boolean,
+      required: true,
+    },
+  },
+  data() {
+    return {
+      user: this.initialUser,
+      isFollowed: this.initialIsFollowed,
+    };
+  },
+  methods: {
+    follow() {
+      this.isFollowed = true;
+    },
+    unFollow() {
+      this.isFollowed = false;
+    },
+  },
+};
+</script>
---
+++ src/router/index.js
@@ -0,0 +1,116 @@
+import Vue from 'vue'
+import VueRouter from 'vue-router'
+import SignIn from '../views/SignIn.vue'
+import NotFound from '../views/NotFound.vue'
+import Restaurants from '../views/Restaurants.vue'
+
+Vue.use(VueRouter)
+
+const routes = [
+  {
+    path: '/',
+    name: 'root',
+    redirect: '/restaurants'
+  },
+  {
+    path: '/signin',
+    name: 'sign-in',
+    component: SignIn
+  },
+  {
+    path: '/signup',
+    name: 'sign-up',
+    component: () => import('../views/SignUp.vue')
+  },
+  {
+    path: '/restaurants',
+    name: 'restaurants',
+    component: Restaurants
+  },
+  {
+    path: '/restaurants/feeds',
+    name: 'restaurants-feeds',
+    component: () => import('../views/RestaurantsFeeds.vue')
+  },
+  {
+    path: '/restaurants/top',
+    name: 'restaurants-tops',
+    component: () => import('../views/RestaurantsTop.vue')
+  },
+  {
+    path: '/restaurants/:id',  //要放在restaurants/feeds和/top後面，因為放前面會把feeds和top誤認為:id
+    name: 'restaurant',
+    component: () => import('../views/Restaurant.vue')
+  },
+  {
+    path: '/restaurants/:id/dashboard',
+    name: 'dashboard',
+    component: () => import('../views/RestaurantDashboard.vue')
+  },
+  {
+    path: '/users/top',
+    name: 'users-tops',
+    component: () => import('../views/UsersTop.vue')
+  },
+  {
+    path: '/users/:id/edit',
+    name: 'user-edit',
+    component: () => import('../views/UserEdit.vue')
+  },
+  {
+    path: '/users/:id',
+    name: 'user',
+    component: () => import('../views/User.vue')
+  },
+  {
+    path: '/admin',
+    exact: true,   //完全匹配
+    redirect: '/admin/restaurants'
+  },
+  {
+    path: '/admin/restaurants',
+    name: 'admin-restaurants',
+    component: () => import('../views/AdminRestaurants.vue')
+  },
+  {
+    path: '/admin/restaurants/new',
+    name: 'admin-restaurant-new',
+    component: () => import('../views/AdminRestaurantNew.vue')
+  },
+  {
+    path: '/admin/categories',
+    name: 'admin-categories',
+    component: () => import('../views/AdminCategories.vue')
+  },
+  {
+    path: '/admin/restaurants/:id/edit',
+    name: 'admin-restaurant-edit',
+    component: () => import('../views/AdminRestaurantEdit.vue'),
+  },
+  {
+    path: '/admin/restaurants/:id',
+    name: 'admin-restaurant',
+    component: () => import('../views/AdminRestaurant.vue'),
+  },
+  {
+    path: '/admin/users',
+    name: 'admin-users',
+    component: () => import('../views/AdminUsers.vue'),
+  },
+  {
+    path: '*',
+    name: 'not-found',
+    component: NotFound
+  }
+]
+
+// const router = new VueRouter({
+//   //mode: 'history',  
+//   //default是hash
+//   routes
+// })
+
+export default new VueRouter({
+  linkExactActiveClass: 'active',
+  routes
+})
